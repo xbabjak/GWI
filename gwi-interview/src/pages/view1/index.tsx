1882f0d4b54b3a57c3f2ea707dff79dcd69d8ea4
radovanBabjak <radovan.babjak@learneron.com>
2024-02-26 20:30:19 +0100

    feat: add parameter of cat id when clicking on a picture

--- gwi-interview/src/pages/view1/index.tsx
+++ gwi-interview/src/pages/view1/index.tsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
+import Link from "next/link";
+import router, { useRouter } from "next/router";
 
 type catData = {
   id: "string";
   url: "string";
   width: number;
   height: number;
+  // breed: []
+  // favourite: boolean;
 };
 
 const View1Page = () => {
@@ -26,16 +30,34 @@ const View1Page = () => {
     fetchData();
   }, []);
 
+  //   router.push({
+  //     pathname: '/signup',
+  //     query: {
+  //        pageId: "page-1"  // update the query param
+  //     }
+  //  }, undefined, { shallow: true})
+
   return (
     <div>
       {catPosts.map((catPost) => (
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src={catPost.url}
-          alt="Next.js Logo"
-          width={200}
-          height={200}
-        />
+        //useMemo
+        <div
+          onClick={() =>
+            router.push({
+              query: {
+                catId: catPost.id, // update the query param
+              },
+            })
+          }
+        >
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src={catPost.url}
+            alt="Next.js Logo"
+            width={200}
+            height={200}
+          />
+        </div>
       ))}
     </div>
   );
