d8a7f57fab3533e8b48031393c0ec709c90c6d12
radovanBabjak <radovan.babjak@learneron.com>
2024-03-01 12:33:29 +0100

    feat: add feedback to favorite form

--- gwi-interview/src/components/CatDetailModal/index.tsx
+++ gwi-interview/src/components/CatDetailModal/index.tsx
@@ -10,6 +10,7 @@ import { api_key } from "@/keys";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 enum FavoriteUpdateStatus {
+  uncalled,
   loading,
   success,
   error,
@@ -24,7 +25,7 @@ export const CatDetailModal = () => {
   const [isCatDetailModalOpen, setIsCatDetailModalOpen] =
     useState<boolean>(false);
   const [favoriteUpdateStatus, setFavoriteUpdateStatus] =
-    useState<FavoriteUpdateStatus>(FavoriteUpdateStatus.success);
+    useState<FavoriteUpdateStatus>(FavoriteUpdateStatus.uncalled);
 
   const catBreeds = catDetail?.breeds;
   const isCatBreedSent = catBreeds?.length && catBreeds.length > 0;
@@ -55,6 +56,7 @@ export const CatDetailModal = () => {
 
   const onSubmit: SubmitHandler<{}> = () => {
     const { catId } = router.query;
+
     axios
       .post(
         `https://api.thecatapi.com/v1/favourites`,
@@ -69,8 +71,10 @@ export const CatDetailModal = () => {
           },
         }
       )
-      .then(() => {
+      .then((success) => {
         setFavoriteUpdateStatus(FavoriteUpdateStatus.success);
+        // remove success
+        console.log("success", success);
       })
       .catch((err) => {
         setFavoriteUpdateStatus(FavoriteUpdateStatus.error);
@@ -91,13 +95,14 @@ export const CatDetailModal = () => {
       // }
     >
       <button
-        onClick={() =>
+        onClick={() => {
+          setFavoriteUpdateStatus(FavoriteUpdateStatus.uncalled);
           router.replace({
             query: {
               catId: "", // delete the query param
             },
-          })
-        }
+          });
+        }}
       >
         Test Exit
       </button>
@@ -141,13 +146,42 @@ export const CatDetailModal = () => {
               </ul>
             </div>
           )}
+          {/* use favorite  */}
           {/* add star filled/empty to describe wheter the cat is favoured already - if the info is in the API */}
-          {/* <form className="yellow"> Favourite </form> */}
-
+          {favoriteUpdateStatus === FavoriteUpdateStatus.success ? (
+            <svg
+              className="w-5 h-5 text-yellow-300 me-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+          ) : (
+            //  star duplicate (only classname was changed)
+            <svg
+              className="w-5 h-5 text-gray-300 me-1 dark:text-gray-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+          )}
           <form onSubmit={handleSubmit(onSubmit)}>
             <button>
-              <input type="submit" />
+              <input
+                type="submit"
+                placeholder="Favourite"
+                // disabled={favoriteUpdateStatus === FavoriteUpdateStatus.success}
+              />
             </button>
+            {/* fill in reason for api fail */}
+            {favoriteUpdateStatus === FavoriteUpdateStatus.error && (
+              <p className="text-red-700">Failed to favourite because of {}</p>
+            )}
           </form>
         </div>
       )}
